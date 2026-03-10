531b5dd3693a9c6c21e562a319e31d1b368855d1
AlKurpiakov <s23380350@unn.ru>
2026-03-10 17:48:07 +0300

    fix: clang-tidy

--- clang/compiler-course/kurpiakov_a_task4/task4.cpp
+++ clang/compiler-course/kurpiakov_a_task4/task4.cpp
@@ -1,6 +1,6 @@
 #include "clang/AST/ASTConsumer.h"
-#include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/AST/Decl.h"
+#include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
 #include "llvm/Support/raw_ostream.h"
@@ -8,43 +8,40 @@
 namespace {
 class VarStatVisitor final : public clang::RecursiveASTVisitor<VarStatVisitor> {
 public:
-  explicit VarStatVisitor(clang::ASTContext *context) : m_context(context), global_var_counter(0), 
-                                                        static_var_counter(0), local_var_counter(0),
-                                                        func_parm_counter(0) {}
-  
-  
+  explicit VarStatVisitor(clang::ASTContext *context)
+      : m_context(context), global_var_counter(0), static_var_counter(0),
+        local_var_counter(0), func_parm_counter(0) {}
+
   bool VisitVarDecl(clang::VarDecl *var) {
 
-    if (var != var->getCanonicalDecl()){
+    if (var != var->getCanonicalDecl()) {
       return true;
     }
 
-    if (var->isFileVarDecl()){
-      if (var->getStorageClass() == clang::SC_Static){
+    if (var->isFileVarDecl()) {
+      if (var->getStorageClass() == clang::SC_Static) {
         static_var_counter += 1;
-      }
-      else {
+      } else {
         global_var_counter += 1;
       }
-    }
-    else if (var->isStaticLocal()){
+    } else if (var->isStaticLocal()) {
       static_var_counter += 1;
-    }
-    else if (var->isLocalVarDeclOrParm()){
-      if (var->isLocalVarDecl()){
+    } else if (var->isLocalVarDeclOrParm()) {
+      if (var->isLocalVarDecl()) {
         local_var_counter += 1;
-      }
-      else {
+      } else {
         func_parm_counter += 1;
       }
     }
 
     return true;
-
   }
 
   void print() const {
-    llvm::outs() << "Total count : " << global_var_counter + static_var_counter + local_var_counter + func_parm_counter<< "\n";
+    llvm::outs() << "Total count : "
+                 << global_var_counter + static_var_counter +
+                        local_var_counter + func_parm_counter
+                 << "\n";
     llvm::outs() << "Global variables : " << global_var_counter << "\n";
     llvm::outs() << "Static variables : " << static_var_counter << "\n";
     llvm::outs() << "Local variables  : " << local_var_counter << "\n";
@@ -57,7 +54,6 @@ private:
   size_t static_var_counter;
   size_t local_var_counter;
   size_t func_parm_counter;
-
 };
 
 class VarStatConsumer final : public clang::ASTConsumer {
@@ -81,7 +77,7 @@ public:
   }
 
   bool ParseArgs(const clang::CompilerInstance &ci,
-                 const std::vector<std::string> &args) override {  
+                 const std::vector<std::string> &args) override {
     return true;
   }
 };
